44de30001425a6ebe5d3eefeb36fbbb641ff9041
Matias Savela <msavela@gmail.com>
2013-12-26 23:28:12 +0200

    Renamed plugins.

--- src/presentate.background.js
+++
@@ -1,6 +0,0 @@
-(function(lecture) {
-	lecture.on('activate', function(e) {
-		lecture.parent.style.background =
-			lecture.deck[e.index].getAttribute('data-background') || 'transparent';
-	});
-}(lecture));
--- src/presentate.progress.js
+++
@@ -1,9 +0,0 @@
-(function(lecture) {
-	lecture.on('activate', function(e) {
-		var percent = ((e.index) / (lecture.deck.length - 1)) * 100;
-		document.querySelector('.progress').style.width = percent + "%";
-		lecture.dispatch('progress', {
-			progress: ((e.index) / (lecture.deck.length - 1)) * 100
-		});
-	});
-}(lecture));
